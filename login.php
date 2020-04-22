6b28e0b0f7445ea492329e1473749425614abd11
Izydemon <43239527+Izydemon@users.noreply.github.com>
2020-04-22 11:45:03 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,42 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-	<!-- Caracteres especiales -->
-	<meta charset="UTF-8">
-
-	<!-- ViewPort -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<!-- Titulo pestana -->
-	<title>GCActiva</title>
-
-	<!-- Favicon -->
-    <link rel="shortcut icon" href="imagenes/favicon.png" type="image/x-icon">
-
-	<!-- Enlace FontAwesome -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
-
-	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Forum&display=swap" rel="stylesheet">
-
-	<!-- Enlaces CSS -->
-	<link rel="stylesheet" href="assets/css/base.css">
-	<link rel="stylesheet" href="assets/css/xs-sm.css">
-	<link rel="stylesheet" href="assets/css/md-lg.css">
-
-	<!-- Enlaces Js -->
-	<script src="assets/js/javascript.js"></script>
-
-</head>
-<body>
-	
-	<?php
-
-	include ("presentation.class.php");
-
+<?php
+	include_once 'business.class.php';
+    include_once 'data_access.class.php';
+    include_once 'presentation.class.php';
+	View::start();
 	View::header();
+
+$login = true;
+
+if (isset($_POST["access"])){
+    $userName = $_POST["name"];
+    $password = $_POST["password"];
+    
+    if(User::login($userName,$password)){
+        header('Location:index.php');
+    } else {
+        $login = false;
+    }
+}
 
 	?>
 
@@ -49,24 +29,25 @@
 		<div class="container content">
 			<div class="margin">
 				<div class="form">
-				    <form class="formlogin" method="POST" action="index.html">
+				    <form class="formlogin" method="POST" action="index.php">
 				        <label class="labelogin" for="nombre">Usuario:</label><br>
-				        <input class="inputlogin" type="text" name="nombre"><br>
+				        <input class="inputlogin" type="text" name="name"><br>
 				        <label class="labelogin" for="password">Contraseña:</label><br>
-				        <input class="inputlogin" type="password" name="contraseña"><br><br>
+				        <input class="inputlogin" type="password" name="password"><br><br>
+				        <input type="text" name="access" value="true" hidden>
 				        <button class="button" type="submit">Identifícate</button><br>
 				    </form>
 				</div>
+				<?php
+                    if (!$login) {
+                        echo "<p><b>Nombre o contraseña incorrectos</b></p>";
+                    }
+                ?>
 			</div>
 		</div>
 	</div>
 </section>
 
-<!-- Footer -->
-<footer>
-	<div class="name">GCActiva</div>
-	<div class="copyright">@Copyright <b>Alexander Alvarez</b> - <b>Isidro Bermúdez</b> - <b>Fabián Beirutti</b></div>
-</footer>
-
-</body>
-</html>
+<?php
+	View::footer();
+?>
