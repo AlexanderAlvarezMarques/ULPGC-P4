5c40f26d1522e9056ccb8377c2236f77d812c3c4
alexanderalvarezmarques <alex222en@gmail.com>
2020-04-23 15:42:15 +0000

    Pagina de perfil

---
+++ perfil.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once ("business.class.php");
+
+$user = Userr::getLoggedUser();
+
+if ($user != null) {
+	switch ($user['tipo']) {
+		case 1:
+			header("Location:partials/administrator.profile.php");
+			break;
+
+		case 2:
+			header("Location:partials/business.profile.php");
+			break;
+		
+		case 3:
+			header("Location:partials/user.profile.php");
+			break;
+
+		default:
+			header("Location:index.php");
+			break;
+	}
+}
