b8704672b56f5108e14107400d8df7dd2861f813
Fabián Alfonso Beirutti Pérez <55493427+Fabbeiru@users.noreply.github.com>
2020-04-23 16:25:52 +0100

    Desarrollo pagina de informacion actividad

--- activity.php
+++ activity.php
@@ -7,8 +7,20 @@ View::start();
 
 View::header();
 
-$nombre = $_GET['nombre'];
+$id = $_GET['id'];
+
+$SQL = "select * from actividades where id='$id';";
+$activity = DB::execute_sql($SQL)->fetchAll(PDO::FETCH_NAMED)[0];
+
+$nombre = $activity['nombre'];
+$tipo = $activity['tipo'];
+$precio = $activity['precio'];
+$aforo = $activity['aforo'];
+$duracion = $activity['duracion'];
+$inicio = $activity['inicio'];
+$img = View::imgtobase64($activity['imagen']);
 
 echo $nombre;
+echo $img;
 
 View::footer();
