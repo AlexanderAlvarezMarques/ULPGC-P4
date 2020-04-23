9ac9a83dfd4a50963c1073de926484f3bd4e9e51
Fabián Alfonso Beirutti Pérez <55493427+Fabbeiru@users.noreply.github.com>
2020-04-23 17:52:18 +0100

    Finished activity further information

--- activity.php
+++ activity.php
@@ -16,11 +16,49 @@ $nombre = $activity['nombre'];
 $tipo = $activity['tipo'];
 $precio = $activity['precio'];
 $aforo = $activity['aforo'];
-$duracion = $activity['duracion'];
-$inicio = $activity['inicio'];
-$img = View::imgtobase64($activity['imagen']);
+$duracion = round($activity['duracion'])/60;
+$inicio = date("d-m-Y H:i:s", $activity['inicio']);
+$descripcion = $activity['descripcion'];
+$idempresa = $activity['idempresa'];
+$imagen = View::imgtobase64($activity['imagen']);
 
-echo $nombre;
-echo $img;
+$SQL = "select nombre from empresas where idempresa='$idempresa';";
+$empresa = DB::execute_sql($SQL)->fetchAll(PDO::FETCH_NAMED)[0]['nombre'];
+?>
+<section class='central'>
+    <div class='container title'>
+	    <h1><?php echo $nombre; ?></h1>
+    </div>
+	<div class='background'>
+	    <div class='container content'>
+	        <div class='margin'>
+                <div class="tour">
+                	<!-- Cuerpo -->
+                	<div class="body">
+                		<!-- Imagen -->
+                		<div class="img">
+                			<img src="<?php echo $imagen ?>">
+                		</div>
+                		
+                		<!-- Descripcion -->
+                		<div class="desc">
+                			<?php echo $descripcion; ?>
+                		</div>
+                
+                		<!-- Info -->
+                		<div class="info">
+                			<div><b>Fecha:</b><div class="value"><?php echo $inicio ?></div></div>
+                			<div><b>Duración:</b><div class="value"><?php echo $duracion ?> min</div></div>
+                			<div><b>Precio:</b><div class="value"><?php echo $precio ?>€</div></div>
+                			<div><b>Empresa:</b><br><a href="empresa.php?name=<?php echo $empresa ?>"><?php echo $empresa ?></a></div>
+                		</div>
+                	</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<?php
 View::footer();
+?>
